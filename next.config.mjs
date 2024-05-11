d1136281688563b93f7d0afe70265058ca1e0370
Zoltan Maroti <zoli.maroti@gmail.com>
2024-05-11 11:39:53 +0200

    config: added svgr loader

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: [
+                {
+                    loader: "@svgr/webpack",
+                    options: {},
+                },
+            ],
+        });
+        return config;
+    },
+};
 
 export default nextConfig;
