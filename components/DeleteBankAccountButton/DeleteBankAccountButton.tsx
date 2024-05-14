ef5222903bb3d1da1bf8b760f0081b6d5c1bbec4
Zoltan Maroti <zoli.maroti@gmail.com>
2024-05-14 21:37:16 +0200

    feat: added tooltip to delete account button

--- components/DeleteBankAccountButton/DeleteBankAccountButton.tsx
+++ components/DeleteBankAccountButton/DeleteBankAccountButton.tsx
@@ -5,6 +5,7 @@ import DeleteIcon from "@/assets/icons/delete.svg";
 import DeleteConfirmationModal from "@/components/DeleteBankAccountButton/DeleteConfirmationModal";
 import { deleteBankAccount } from "@/components/DeleteBankAccountButton/actions";
 import { BankAccount } from "@/components/BankAccountCard/types";
+import { Tooltip } from "react-tooltip";
 
 const DeleteBankAccountButton = ({
     bankAccount,
@@ -30,10 +31,18 @@ const DeleteBankAccountButton = ({
                 type='button'
                 onClick={toggleModal}
                 disabled={isDisabled}
+                data-tooltip-id='delete-tooltip'
+                data-tooltip-place='bottom'
+                data-tooltip-content={
+                    isDisabled
+                        ? "You can't delete your account with a balance"
+                        : "Delete account"
+                }
                 className='focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-1 disabled:bg-gray-300 disabled:cursor-not-allowed'
             >
                 <DeleteIcon className='w-5 h-5' />
             </button>
+            <Tooltip id='delete-tooltip' className='!px-2 !py-1' opacity={1} />
         </>
     );
 };
