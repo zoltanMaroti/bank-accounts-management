897cc12f538a5ae73f4b9ccaa731daee6b13f617
Zoltan Maroti <zoli.maroti@gmail.com>
2024-05-15 13:09:02 +0200

    feat: added source and destination account selectors

--- components/TransferFundsForm/TransferFundsForm.tsx
+++ components/TransferFundsForm/TransferFundsForm.tsx
@@ -1,11 +1,26 @@
 "use client";
 
-import React from "react";
+import React, { useState, useMemo, ChangeEvent } from "react";
 import MoneyIcon from "@/assets/icons/money.svg";
 import CurrencySelector from "@/components/CurrencySelector/CurrencySelector";
 import { BankAccount } from "@/components/BankAccountCard/types";
+import Select from "@/components/Select/Select";
 
 const TransferFundsForm = ({ accounts }: { accounts: BankAccount[] }) => {
+    const [sourceAccountId, setSourceAccountId] = useState<string>();
+    const [destinationAccountId, setDestinationAccountId] = useState<string>();
+
+    const onChangeFromAccount = (e: ChangeEvent<HTMLSelectElement>) =>
+        setSourceAccountId(e.target.value);
+
+    const onChangeToAccount = (e: ChangeEvent<HTMLSelectElement>) =>
+        setDestinationAccountId(e.target.value);
+
+    const eligibleDestinationAccounts = useMemo(
+        () => accounts.filter((account) => account.id !== sourceAccountId),
+        [accounts, sourceAccountId]
+    );
+
     return (
         <form className='relative bg-white rounded-md p-4 mt-6 w-full flex flex-col gap-3'>
             <h1 className='flex-1 text-xl font-bold text-center'>
@@ -13,6 +28,40 @@ const TransferFundsForm = ({ accounts }: { accounts: BankAccount[] }) => {
             </h1>
             <p>Deposit funds into an account</p>
             <hr className='border mb-2' />
+
+            <Select
+                id='from'
+                label='Transfer from'
+                hasError={false}
+                errorMessage={""}
+                onChange={onChangeFromAccount}
+            >
+                <option value=''>Please choose an option</option>
+                {accounts.map((account) => (
+                    <option key={account.id} value={account.id}>
+                        {account.currency}: {account.accountType} -{" "}
+                        {account.description}
+                    </option>
+                ))}
+            </Select>
+
+            {sourceAccountId ? (
+                <Select
+                    id='from'
+                    label='Transfer to'
+                    hasError={false}
+                    errorMessage={""}
+                    onChange={onChangeToAccount}
+                >
+                    <option value=''>Please choose an option</option>
+                    {eligibleDestinationAccounts.map((account) => (
+                        <option key={account.id} value={account.id}>
+                            {account.currency}: {account.accountType} -{" "}
+                            {account.description}
+                        </option>
+                    ))}
+                </Select>
+            ) : null}
 
             <div className='w-full mx-auto flex'>
                 <div className='relative w-full'>
