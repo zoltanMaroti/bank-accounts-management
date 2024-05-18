6d20a2bfc160f5c494fbd65151a3c4fe8edf3830
Zoltan Maroti <zoli.maroti@gmail.com>
2024-05-18 14:57:07 +0200

    feat: debounce search to throttle requests

--- components/BankAccounts/hooks.tsx
+++ components/BankAccounts/hooks.tsx
@@ -10,10 +10,22 @@ import {
 import { BankAccount } from "@/components/BankAccountCard/types";
 import { searchAccount } from "@/components/BankAccounts/actions";
 
+const useDebounce = <T,>(value: T, delay: number = 300): T => {
+    const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+    useEffect(() => {
+        const debounce = setTimeout(() => setDebouncedValue(value), delay);
+        return () => clearTimeout(debounce);
+    }, [value, delay]);
+
+    return debouncedValue;
+};
+
 export const useSearch = () => {
+    const [isPending, startTransition] = useTransition();
     const [searchResult, setSearchResult] = useState<BankAccount[]>([]);
     const [searchTerm, setSearchTerm] = useState("");
-    const [isPending, startTransition] = useTransition();
+    const debouncedSearchTerm = useDebounce(searchTerm);
 
     const onChangeSearchTerm = useCallback(
         (e: ChangeEvent<HTMLInputElement>) => {
@@ -39,12 +51,12 @@ export const useSearch = () => {
             setSearchResult(accounts);
         };
 
-        if (searchTerm.length) {
+        if (debouncedSearchTerm.length) {
             startTransition(async () => {
-                await search(searchTerm);
+                await search(debouncedSearchTerm);
             });
         }
-    }, [searchTerm]);
+    }, [debouncedSearchTerm]);
 
     return {
         onChangeSearchTerm,
